ee600eade7203c8c5670fe7b1eaf241cf898b29d
Brandon Ojeda <brandono9192@gmail.com>
2025-03-07 15:19:40 -0500

    added content to BLANK tab

--- app/(tabs)/BLANK.js
+++ app/(tabs)/BLANK.js
@@ -1,1 +1,22 @@
 //blank page, which can be navigated to at bottom of the page
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function blankScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Home</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
